eea96054375c897aafb37739a5c1309995461acb
sgshn4 <davidov.oleg2018@yandex.ru>
2023-09-04 04:01:38 +0300

    Commited to GitHub. Created MainWindow.

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MainWindow mainWindow = new MainWindow();
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainWindow.setSize(800, 600);
+        mainWindow.setLocationRelativeTo(null);
+        mainWindow.setVisible(true);
+    }
+}
